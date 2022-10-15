2a1189c273cd701c20c03038f6a7d17872394422
rafael-takano <rafaelkuhntakano@hotmail.com>
2022-10-15 00:45:21 -0300

    Implemented Best First Search, probably has bugs same as bfs and dfs

--- search_algorithms.py
+++ search_algorithms.py
@@ -6,7 +6,8 @@ def invert_list(list):
     while len(list) > 1: 
         list_end.append(list[-1])
         list = list[0]
-    list_end.append(list[0])
+    if list:        
+        list_end.append(list[0])
     return list_end
 
 def dist_between_vertices(v_list, v1, v2): 
@@ -63,6 +64,49 @@ def BFS(v_list, e_list, v_start, v_end):
 def Best_First(v_list, e_list, v_start, v_end): 
     path = []
     total_dist = 0
+
+    visited = np.zeros(len(v_list))    
+
+    p_queue = [[dist_between_vertices(v_list,v_start,v_end),v_start]]
+
+    memo = []
+    
+    visited[v_start] = 1
+
+    while p_queue:
+        u = p_queue.pop()
+        for w in e_list[u[1]]:
+            if(not visited[w]): 
+                p_queue.append([dist_between_vertices(v_list,w,v_end),w])
+                memo.append([u[1],w])
+                visited[w] = 1    
+        
+        p_queue.sort(reverse=True)
+        if(visited[v_end]):
+            break
+
+    path = [v_end]
+    for memory in reversed(memo): 
+        if(path[-1] == memory[1]): 
+            total_dist += dist_between_vertices(v_list, path[-1], memory[0])
+            path = [path,memory[0]]
+
+    return invert_list(path), total_dist
+    """procedure GBS(start, target) is:
+        mark start as visited
+        add start to queue
+        while queue is not empty do:
+            current_node ← vertex of queue with min distance to target
+            remove current_node from queue
+            foreach neighbor n of current_node do:
+            if n not in visited then:
+                if n is target:
+                return n
+                else:
+                mark n as visited
+                add n to queue
+        return failure 
+    """
     return path, total_dist
 
 def A_algorithm(v_list, e_list, v_start, v_end): 
